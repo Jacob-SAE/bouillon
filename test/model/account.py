f6321240f27ea4cc1f26c01a99393848466a3221
Jacob-SAE <jacob@sharpauctionengine.com>
2015-07-15 11:46:54 -0500

    update

--- test/model/account.py
+++ test/model/account.py
@@ -5,9 +5,13 @@ from sqlalchemy.orm import relationship
 class Account(Base):
     __tablename__ = 'accounts'
     
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(128), nullable=False)
+    first_name = db.Column(db.String(128), nullable=False)
+    last_name = db.Column(db.String(128), nullable=False)
+    full_name = first_name + last_name
     email = db.Column(db.String(128), nullable=False, unique=True)
+    
+
+    products = relationship("Product", backref="account")
 
     def __repr__(self):
         return "<%s|%d: %s (%s)>" % (self.uuid, self.id, self.email, self.name)
